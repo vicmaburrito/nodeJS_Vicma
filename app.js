4ea9d952596919baa5734d1a40ad015734cbdedf
vicmaburrito <65370617+vicmaburrito@users.noreply.github.com>
2021-09-15 18:26:05 -0500

    Create app.js & add all logic

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const twilio = require("./twilio");
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/webhook', function (req, res) {
-  res.send('Hello World')
+    console.log("req ->", req.body);
+    twilio.sendTextMessage(req.body.WaId, req.body.Body);
+    res.status(200).json({ ok: true, msg: "Mensaje enviado correctamente" });
 });
  
 app.listen(3000,()=> {
