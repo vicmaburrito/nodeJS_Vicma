b22ed01d498ac2f31c1f292cac9ff911ab047d2d
vicmaburrito <65370617+vicmaburrito@users.noreply.github.com>
2021-09-16 14:20:58 -0500

    Add dialogflow dependecies

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const twilio = require("./twilio");
+const dialogflow = require("./dialogflow");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/webhook', function (req, res) {
-    console.log("req ->", req.body);
+app.post('/webhook', async function (req, res) {
+    // console.log("req ->", req.body);
+    // let receivedMessage = req.body.Body;
+    // let payload = await dialogflow.sendToDialogFlow(message.body, "vicma");
+    // let responses = payload.fulfillmentMessages;
+    // for (const response of responses) {
+    //   await sendMessageToWhatsapp(client, message, response);
+    // }
+
     twilio.sendTextMessage(req.body.WaId, req.body.Body);
     res.status(200).json({ ok: true, msg: "Mensaje enviado correctamente" });
 });
